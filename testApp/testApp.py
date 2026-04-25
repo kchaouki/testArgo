16094054492435bcc76f90a6400297035f4aa33c
kchaouki <karimchaouki50@gmail.com>
2026-04-25 18:43:27 +0100

    add dev namespace manifests and v1/v2 versioned app

--- testApp/testApp.py
+++ testApp/testApp.py
@@ -1,10 +1,15 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+VERSION = "v1"
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-type", "application/json")
         self.end_headers()
-        self.wfile.write(b"Hello, I am App1  .")
+        self.wfile.write(f'{{"status":"ok", "message": "{VERSION}"}}'.encode())
+
+    def log_message(self, format, *args):
+        pass
 
 HTTPServer(("0.0.0.0", 80), Handler).serve_forever()
